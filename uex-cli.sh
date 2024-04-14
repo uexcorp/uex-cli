4741085b1eacbb7b4f24e4cfba5d4bc94f8bcd1f
UEX <zatec@uexcorp.space>
2024-04-14 16:48:33 -0300

    Update uex-cli.sh

--- uex-cli.sh
+++ uex-cli.sh
@@ -6,9 +6,9 @@
 environment=""
 secret_key=""
 
-uex_api_url_local="http://localhost/uex/uex-v2/cli/"
-uex_api_url_ptu="https://ptu.uexcorp.space/cli/"
-uex_api_url_production="https://uexcorp.space/cli/"
+uex_api_url_local="http://localhost/uex/uex-v2/cli/input/"
+uex_api_url_ptu="https://ptu.uexcorp.space/cli/input/"
+uex_api_url_production="https://uexcorp.space/cli/input/"
 
 secret_file_key="$HOME/.uexkey"
 secret_file_env="$HOME/.uexenv"
